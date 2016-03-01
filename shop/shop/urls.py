255102db7c9bf6483b0bebf464e7b8649a77afb0
Alexander Zaytsev <thebestzorro@yandex.ru>
2016-03-01 23:45:32 +0300

    admin doc

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 import sales.views as sales
 
@@ -21,4 +21,5 @@ urlpatterns = [
     url(r'^$', sales.index, name='index'),
     # admin interface
     url(r'^admin/', admin.site.urls),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 ]
